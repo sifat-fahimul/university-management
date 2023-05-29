056e3631833b060ae9de3b91a8df77863a90e925
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-05-29 11:22:43 +0600

    [DONE] setup prettier eslint

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string)
-        console.log('Database connected')
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected')
 
-        app.listen(config.prot,()=>{
-            console.log(`Server running on port ${config.prot}`)
-        });
-    }catch(err){
-        console.log('Failed to connect DB: ', err)
-    }
+    app.listen(config.prot, () => {
+      console.log(`Server running on port ${config.prot}`)
+    })
+  } catch (err) {
+    console.log('Failed to connect DB: ', err)
+  }
 }
 
-bootstrap();
+bootstrap()
