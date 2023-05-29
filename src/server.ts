ac3a526f0676d44d09120361102d49310d4ac059
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-05-29 12:25:22 +0600

    setup eslint prettier husky lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
