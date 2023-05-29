5ef3e24de970457e1a5c869535791275cb7a3278
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-05-29 17:02:34 +0600

    controller route and create user api done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -9,8 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+app.use('/api/v1/users/', usersRouter)
+
 // testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Everything is good')
 })
 
