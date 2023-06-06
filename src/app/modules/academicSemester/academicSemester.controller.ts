217d2f05adaf1fb30621da365b10df4cce701ccc
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-06-06 11:29:42 +0600

    optomize route controller try-catch and api response

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,20 +1,24 @@
-import { RequestHandler } from 'express';
 import { AcademicSemestersService } from './academicSemester.service';
+import catchAsync from '../../../shared/catchAsync';
+import { NextFunction, Request, Response } from 'express';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
 
-const createAcademicSemester: RequestHandler = async (req, res, next) => {
-  try {
+const createAcademicSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...payload } = req.body;
-
     const result = await AcademicSemestersService.createSemester(payload);
-    res.status(200).json({
+
+    next();
+
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
       success: true,
       message: 'Academic semester created successfully!',
       data: result,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 export const AcademicSemesterController = {
   createAcademicSemester,
